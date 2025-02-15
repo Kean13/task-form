ffb563684b9fc69892515ceb54c0f76ecc7b7e2e
kean <kean@octo.gg>
2025-02-15 11:00:04 +0500

    ♻️ Чистка кода

--- src/components/layout/Create/CreateTagInput.tsx
+++ src/components/layout/Create/CreateTagInput.tsx
@@ -2,8 +2,8 @@
 
 import { KeyboardEvent, useState } from 'react'
 import { Input } from '@/components/default/Input'
-import { Trash2, Plus } from 'lucide-react' // Импортируем иконку Plus
-import { Button } from '@/components/default/Button' // Импортируем компонент Button
+import { Trash2, Plus } from 'lucide-react'
+import { Button } from '@/components/default/Button'
 
 interface TagInputProps {
   tags: string[]
@@ -13,15 +13,13 @@ interface TagInputProps {
 export const CreateTagInput = ({ tags, setTags }: TagInputProps) => {
   const [tagInput, setTagInput] = useState('')
 
-  // Функция для добавления тега
   const addTag = () => {
     if (tagInput.trim() !== '') {
       setTags([...tags, tagInput.trim()])
-      setTagInput('') // Очищаем поле ввода
+      setTagInput('')
     }
   }
 
-  // Обработка нажатия Enter
   const handleTagKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter' && tagInput.trim() !== '') {
       event.preventDefault()
@@ -29,7 +27,6 @@ export const CreateTagInput = ({ tags, setTags }: TagInputProps) => {
     }
   }
 
-  // Удаление тега
   const removeTag = (tag: string) => {
     setTags(tags.filter(t => t !== tag))
   }
@@ -64,12 +61,7 @@ export const CreateTagInput = ({ tags, setTags }: TagInputProps) => {
           onKeyDown={handleTagKeyPress}
           className='placeholder:text-foreground placeholder:opacity-30 p-0 pb-6 pt-10 rounded-none bg-transparent text-lg border-0 border-b-[0.5px] border-b-border flex-1'
         />
-        <Button
-          size='icon'
-          onClick={addTag}
-          className='p-2 rounded-full hover:bg-primary/10 transition-colors'
-          disabled={tagInput.trim() === ''}
-        >
+        <Button onClick={addTag} disabled={tagInput.trim() === ''}>
           <Plus className='w-6 h-6' />
         </Button>
       </div>
